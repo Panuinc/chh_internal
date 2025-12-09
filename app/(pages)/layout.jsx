0290767de2cabdcdef38b77b7b702cf01f20ab31
Panuinc <panuwat.ja.forwork@gmail.com>
2025-12-09 13:51:50 +0700

    ีupdate

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -4,27 +4,27 @@ import Image from "next/image";
 export default function PagesLayout({ children }) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full gap-2">
-      <div className="flex flex-row items-center justify-between w-full h-fit p-2 gap-2 border-b-2 border-default">
+      <div className="flex flex-row items-center justify-between w-full h-fit p-2 gap-2 bg-foreground text-foreground">
         <div className="flex items-center justify-start w-fit xl:min-w-60 h-full p-2 gap-2">
-          <Image src="/logo/logo-04.png" alt="logo" width={125} height={125} />
+          <Image src="/logo/logo-08.png" alt="logo" width={125} height={125} />
         </div>
         <div className="xl:flex items-center justify-center w-full h-full p-2 gap-2 hidden">
           {" "}
         </div>
-        <div className="xl:flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-2 border-foreground bg-foreground/75 text-background rounded-full hidden">
+        <div className="xl:flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-1 border-background bg-background rounded-full hidden">
           P
         </div>
-        <div className="flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-2 border-foreground bg-foreground/75 text-background rounded-full">
+        <div className="flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-1 border-background bg-background rounded-full">
           <BellDot />
         </div>
-        <div className="flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-2 border-foreground bg-foreground/75 text-background rounded-full">
+        <div className="flex items-center justify-center w-12 xl:w-14 aspect-square p-2 gap-2 border-1 border-background bg-background rounded-full">
           <Key />
         </div>
       </div>
       <div className="flex items-center justify-center w-full h-full gap-2 overflow-hidden">
         {children}
       </div>
-      <div className="flex flex-row items-center justify-center w-full h-fit p-2 gap-2 border-t-2 border-default">
+      <div className="flex flex-row items-center justify-center w-full h-fit p-2 gap-2 bg-foreground text-background">
         <div className="flex items-center justify-center w-full h-full p-2 gap-2">
           EVERGREEN BY CHH
         </div>
--- module/home/UIHome.jsx
+++ module/home/UIHome.jsx
@@ -22,23 +22,23 @@ function HeaderMenu({ icons, text, href }) {
 export default function UIHome() {
   return (
     <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2">
-      <div className="flex flex-col items-center justify-center w-full xl:w-3/12 h-fit xl:h-full gap-2">
-        <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-warning bg-warning/75 text-background rounded-xl">
+      <div className="flex flex-col items-center justify-center w-full xl:w-2/12 h-fit xl:h-full gap-2">
+        <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-1 border-success bg-success text-background rounded-xl">
           <Image src="/images/images.png" alt="logo" width={200} height={200} />
         </div>
-        <div className="xl:flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-foreground bg-foreground/75 text-background rounded-xl hidden">
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 text-4xl">
+        <div className="xl:flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-1 border-foreground bg-foreground text-background rounded-xl hidden">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 text-2xl">
             <Ghost /> Welcome Back
           </div>
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 text-xl">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 text-xl">
             Panuwat Jangchudjai
           </div>
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2">
             Description
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 xl:grid-cols-3 place-items-center w-full xl:w-9/12 h-full p-2 gap-2 border-2 border-default rounded-xl overflow-auto">
+      <div className="grid grid-cols-2 xl:grid-cols-3 place-items-center w-full xl:w-8/12 h-full p-2 gap-2 border-1 border-foreground rounded-xl overflow-auto">
         <HeaderMenu href="hr" icons={<User />} text="HR" />
         <HeaderMenu href="hr" icons={<User />} text="HR" />
         <HeaderMenu href="hr" icons={<User />} text="HR" />
