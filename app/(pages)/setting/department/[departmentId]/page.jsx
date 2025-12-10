5fedda6be2dbb46250e096b3dfc4cddbdc0892b2
Panuinc <panuwat.ja.forwork@gmail.com>
2025-12-10 07:51:02 +0700

    update

---
+++ app/(pages)/setting/department/[departmentId]/page.jsx
@@ -0,0 +1,6 @@
+import UIDepartmentForm from "@/module/setting/department/UIDepartmentForm";
+import React from "react";
+
+export default function departmentUpdate() {
+  return <UIDepartmentForm />;
+}
