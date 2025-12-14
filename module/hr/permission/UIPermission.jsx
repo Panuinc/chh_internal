4e4c010750f39f420da29c4dcc56789c4d00f935
Panuinc <panuwat.ja.forwork@gmail.com>
2025-12-14 21:19:04 +0700

    update

--- module/hr/permission/UIPermission.jsx
+++ module/hr/permission/UIPermission.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function UIPermission() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-start w-full xl:w-10/12 h-full p-2 gap-2 border overflow-auto">
+      <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border">
+        1
+      </div>
+      <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border">
+        2
+      </div>
+      <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border">
+        3
+      </div>
     </div>
-  )
+  );
 }
