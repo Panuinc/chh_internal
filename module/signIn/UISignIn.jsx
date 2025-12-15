5dc9bdc64da5c33053c74ae5202cf7325c74d8e6
Panuinc <panuwat.ja.forwork@gmail.com>
2025-12-15 16:01:38 +0700

    update

--- module/signIn/UISignIn.jsx
+++ module/signIn/UISignIn.jsx
@@ -12,9 +12,24 @@ export default function UISignIn({
 }) {
   return (
     <div className="flex flex-row items-center justify-center w-full h-full">
-      <div className="flex flex-col items-center justify-center w-full xl:w-4/12 h-full p-2 gap-2">
+      <div className="flex flex-col items-center justify-center w-full xl:w-4/12 h-full p-2 gap-2 xl:border-r-2">
         <div className="flex items-end justify-center w-full h-fit p-2 gap-2 text-3xl font-black">
-          Ever<span className="text-primary text-6xl">Green</span> Internal
+          Ever
+          <span className="text-primary text-6xl">
+            G
+            <span className="relative inline-block">
+              r
+              <svg
+                className="absolute -top-4 -right-1 w-5 h-5 text-green-600"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M17,8C8,10 5.9,16.17 3.82,21.34L5.71,22L6.66,19.7C7.14,19.87 7.64,20 8,20C19,20 22,3 22,3C21,5 14,5.25 9,6.25C4,7.25 2,11.5 2,13.5C2,15.5 3.75,17.25 3.75,17.25C7,8 17,8 17,8Z" />
+              </svg>
+            </span>
+            een
+          </span>
+          Internal
         </div>
 
         <form
@@ -71,8 +86,8 @@ export default function UISignIn({
         </form>
       </div>
 
-      <div className="xl:flex items-center justify-center xl:w-8/12 h-full p-2 gap-2 hidden bg-primary">
-        <Image src="/logo/logo-05.png" alt="logo" width={300} height={300} />
+      <div className="xl:flex items-center justify-center xl:w-8/12 h-full p-2 gap-2 hidden">
+        <Image src="/logo/logo-01.png" alt="logo" width={300} height={300} />
       </div>
     </div>
   );
