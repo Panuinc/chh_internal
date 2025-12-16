a68fe51f7d39bb9dc555e2463a65714d516980d3
Panuinc <panuwat.ja.forwork@gmail.com>
2025-12-16 13:24:46 +0700

    update

--- components/LoadingState.jsx
+++ components/LoadingState.jsx
@@ -4,7 +4,7 @@ import { Spinner } from "@heroui/react";
 export default function LoadingState({ label = "Loading..." }) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2">
-      <Spinner color="default" variant="wave" size="lg" />
+      <Spinner color="primary" variant="wave" size="lg" />
       <span>{label}</span>
     </div>
   );
