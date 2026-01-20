baf50efdd57aacea7457ec55d007b9c1870cf92b
Panuinc <panuwat.ja.forwork@gmail.com>
2026-01-20 16:02:48 +0700

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
         `}
       >
         <Providers>
-          <div className="flex items-center justify-center w-full h-screen gap-2 text-sm">
+          <div className="flex items-center justify-center w-full h-screen gap-2 bg-background text-foreground">
             {children}
           </div>
         </Providers>
